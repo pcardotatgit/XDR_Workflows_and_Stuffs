0c05be7c881aea9a16d04b01542b47ee686b7e5b
Patrick Cardot <patrick.cardot@gmail.com>
2021-04-18 18:44:28 +0200

    readme_update

---
+++ 6-relay_modules_for_tiles/schemas.py
@@ -0,0 +1,45 @@
+from marshmallow import INCLUDE, Schema, ValidationError, fields
+
+
+def validate_string(value):
+    if value == "":
+        raise ValidationError("Field may not be blank.")
+
+
+class ObservableSchema(Schema):
+    type = fields.String(
+        validate=validate_string,
+        required=True,
+    )
+    value = fields.String(
+        validate=validate_string,
+        required=True,
+    )
+
+
+class ActionFormParamsSchema(Schema):
+    action_id = fields.String(
+        data_key="action_id",
+        validate=validate_string,
+        required=True,
+    )
+    observable_type = fields.String(
+        validate=validate_string,
+        required=True,
+    )
+    observable_value = fields.String(
+        validate=validate_string,
+        required=True,
+    )
+
+    class Meta:
+        unknown = INCLUDE
+
+
+class DashboardTileSchema(Schema):
+    tile_id = fields.String(data_key="tile_id", validate=validate_string, required=True)
+
+
+class DashboardTileDataSchema(Schema):
+    period = fields.String(data_key="period", validate=validate_string, required=True)
+    tile_id = fields.String(data_key="tile_id", validate=validate_string, required=True)
---
+++ 6-relay_modules_for_tiles/utils.py
@@ -0,0 +1,77 @@
+from authlib.jose import jwt
+from authlib.jose.errors import BadSignatureError, DecodeError
+from flask import current_app, jsonify, request
+
+from errors import AuthorizationError, InvalidArgumentError
+
+
+def get_auth_token():
+    """
+    Parse and validate incoming request Authorization header.
+
+    NOTE. This function is just an example of how one can read and check
+    anything before passing to an API endpoint, and thus it may be modified in
+    any way, replaced by another function, or even removed from the module.
+    """
+    expected_errors = {
+        KeyError: 'Authorization header is missing',
+        AssertionError: 'Wrong authorization type'
+    }
+    try:
+        scheme, token = request.headers['Authorization'].split()
+        assert scheme.lower() == 'bearer'
+        return token
+    except tuple(expected_errors) as error:
+        raise AuthorizationError(expected_errors[error.__class__])
+
+
+def get_jwt():
+    """
+    Parse the incoming request's Authorization Bearer JWT for some credentials.
+    Validate its signature against the application's secret key.
+
+    NOTE. This function is just an example of how one can read and check
+    anything before passing to an API endpoint, and thus it may be modified in
+    any way, replaced by another function, or even removed from the module.
+    """
+
+    expected_errors = {
+        KeyError: 'Wrong JWT payload structure',
+        TypeError: '<SECRET_KEY> is missing',
+        BadSignatureError: 'Failed to decode JWT with provided key',
+        DecodeError: 'Wrong JWT structure'
+    }
+    token = get_auth_token()
+    try:
+        return ""
+    except tuple(expected_errors) as error:
+        raise AuthorizationError(expected_errors[error.__class__])
+
+
+def get_json(schema):
+    """
+    Parse the incoming request's data as JSON.
+    Validate it against the specified schema.
+
+    NOTE. This function is just an example of how one can read and check
+    anything before passing to an API endpoint, and thus it may be modified in
+    any way, replaced by another function, or even removed from the module.
+    """
+
+    data = request.get_json(force=True, silent=True, cache=False)
+
+    '''
+    message = schema.validate(data)
+
+    if message:
+        raise InvalidArgumentError(message)
+    '''
+    return data
+
+
+def jsonify_data(data):
+    return jsonify({'data': data})
+
+
+def jsonify_errors(data):
+    return jsonify({'errors': [data]})
